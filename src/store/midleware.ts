f22690ffaf168d26d0e6448fc28774f4b1dcbd33
qnhat <dangquangnhat1504@gmail.com>
2026-08-31 21:33:03 +0700

    fix(store): prevent undefined toast in rtkQueryErrorLogger

--- src/store/midleware.ts
+++ src/store/midleware.ts
@@ -1,4 +1,3 @@
-import { i18nTranslator } from "@/services/i18n";
 import { isRejectedWithValue } from "@reduxjs/toolkit";
 import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
 import { message } from "antd";
@@ -6,7 +5,11 @@ import { message } from "antd";
 export const rtkQueryErrorLogger: Middleware =
   (api: MiddlewareAPI) => (next) => (action: any) => {
     if (isRejectedWithValue(action)) {
-      message.error(i18nTranslator(`${action?.payload?.data?.code}`));
+      const code = action?.payload?.data?.code;
+      const serverMsg = action?.payload?.data?.message;
+      if (code && serverMsg) {
+        message.error(serverMsg);
+      }
     }
 
     return next(action);
